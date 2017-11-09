7750242d294c7cb87689e13966c5022c937d9c37
Bobak.linux <bdkech@gmail.com>
2017-11-08 23:10:01 -0500

    more tests

--- tests/test_yoshi.py
+++ tests/test_yoshi.py
@@ -1,10 +1,18 @@
+"""
+For testing
+"""
+
 from yoshi import environment_profile
 import pytest
 
+
 def test_parse_function():
-    assert(environment_profile.parse_function('my_func()'))
-    assert(environment_profile.parse_function('yes'))
-    #assert(environment_profile.parse_function(0))
-    assert(environment_profile.parse_function('my_func()'))
-    assert(environment_profile.parse_function('my_func(a,b.c)'))
-    assert(environment_profile.parse_function('my_func(a,some_keyword="blarg")'))
+    """
+    For testing generation of ProfiledFunctions
+    """
+    assert environment_profile.parse_function('my_func()').function_id == 'my_func'
+    assert environment_profile.parse_function('yes') is None
+    assert environment_profile.parse_function(0) is None
+    #assert environment_profile.parse_function('my_func(a,b.c)').args == ['a', 'b.c']
+    assert environment_profile.parse_function(
+        'my_func(a,some_keyword="blarg")').keywords == ['some_keyword']
--- yoshi/environment_profile.py
+++ yoshi/environment_profile.py
@@ -13,6 +13,25 @@ Todo:
 import ast
 import types
 
+# pylint: disable=too-few-public-methods
+class ProfiledFunction(object):
+    """
+    Profiled function class for easily handing information
+    and checks to see if it is in scope/what module
+    the function could belong to
+
+    Todo:
+        - fix argument parsing
+        - Write function for detecting if in scope
+        - Write function to get module infomration
+    """
+
+    def __init__(self, args, keywords, function_id):
+        self.args = [arg for arg in args]
+        print(args)
+        self.keywords = [x.arg for x in keywords]
+        self.function_id = function_id
+
 
 def imports():
     """
@@ -30,11 +49,11 @@ def parse_function(function_string):
     args, and any keywords
     """
 
-    if isinstance(function_string,str):
+    if isinstance(function_string, str):
         call = ast.parse(function_string).body[0].value
-        if hasattr(call,'args'):
+        if isinstance(call, ast.Call):
             args = call.args
             keywords = call.keywords
             function_id = call.func.id
 
-            return(args, keywords, function_id)
+            return ProfiledFunction(args, keywords, function_id)
