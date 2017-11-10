1290012e84a5427c0435733f9dd20930306dfbed
Bobak.linux <bdkech@gmail.com>
2017-11-09 22:32:35 -0500

    finished OOO approach, added code for checking import status

--- yoshi/environment_profile.py
+++ yoshi/environment_profile.py
@@ -12,6 +12,33 @@ Todo:
 
 import ast
 import types
+import builtins
+
+def get_arg_label(my_arg):
+    """
+    Get arguments from ast parsing and their type
+    return their text representation, either a variable
+    or string
+    """
+
+    # Args can either be named objects or inputted
+    # strings determine which and give back the their name
+    # This is an object, has an id for a label
+    if hasattr(my_arg, "id"):
+        return my_arg.id
+    # This is a string, use the text as the label
+    elif hasattr(my_arg, "s"):
+        return my_arg.s
+
+
+def imports():
+    """
+    Get a list of the currently imported modules
+    """
+
+    for _, val in globals().items():
+        if isinstance(val, types.ModuleType):
+            yield val.__name__
 
 # pylint: disable=too-few-public-methods
 class ProfiledFunction(object):
@@ -27,20 +54,24 @@ class ProfiledFunction(object):
     """
 
     def __init__(self, args, keywords, function_id):
-        self.args = [arg for arg in args]
-        print(args)
-        self.keywords = [x.arg for x in keywords]
+        self.args = [get_arg_label(arg) for arg in args]
+        # check if args are strings or names, then store them
+        # as necessary
+        kword_tup = [(x.arg, x.value.s) for x in keywords]
+        self.keywords = dict((x, y) for x, y in kword_tup)
         self.function_id = function_id
 
+    def check_import_status(self):
+        """
+        Check whether or not the function is part of an imported
+        module in scope
+        """
 
-def imports():
-    """
-    Get a list of the currently imported modules
-    """
+        print([x for x in imports()])
+        if builtins.any(hasattr(module_name, self.function_id) for module_name in imports()):
+            return True
+        return False
 
-    for _, val in globals().items():
-        if isinstance(val, types.ModuleType):
-            yield val.__name__
 
 
 def parse_function(function_string):
@@ -54,6 +85,9 @@ def parse_function(function_string):
         if isinstance(call, ast.Call):
             args = call.args
             keywords = call.keywords
-            function_id = call.func.id
+            if hasattr(call.func, 'id'):
+                function_id = call.func.id
+            elif hasattr(call.func, 'attr'):
+                function_id = call.func.attr
 
             return ProfiledFunction(args, keywords, function_id)
