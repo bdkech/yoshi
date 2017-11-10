40b50f650e64f0d68d5a2b47822cc4058b47fd80
Bobak.linux <bdkech@gmail.com>
2017-11-09 22:32:11 -0500

    updated tests for checking if function is imported

--- tests/test_yoshi.py
+++ tests/test_yoshi.py
@@ -4,7 +4,7 @@ For testing
 
 from yoshi import environment_profile
 import pytest
-
+import pandas
 
 def test_parse_function():
     """
@@ -13,6 +13,9 @@ def test_parse_function():
     assert environment_profile.parse_function('my_func()').function_id == 'my_func'
     assert environment_profile.parse_function('yes') is None
     assert environment_profile.parse_function(0) is None
-    #assert environment_profile.parse_function('my_func(a,b.c)').args == ['a', 'b.c']
+    assert environment_profile.parse_function('my_func(a,"doggie")').args == ['a', 'doggie']
+    assert list(environment_profile.parse_function(
+        'my_func(a,some_keyword="blarg")').keywords.keys()) == ['some_keyword']
     assert environment_profile.parse_function(
-        'my_func(a,some_keyword="blarg")').keywords == ['some_keyword']
+        'my_func(a,some_keyword="blarg")').keywords['some_keyword'] == 'blarg'
+    assert environment_profile.parse_function('pandas.read_csv()').check_import_status() is True
