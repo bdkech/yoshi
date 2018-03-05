059107b5152b8ea482ad836135b93dccbf270532
Bobak Kechavarzi <bdkech@Bobaks-MacBook-Pro.local>
2018-03-04 22:15:10 -0500

    cleaning up profiling functions

--- yoshi/environment_profile.py
+++ yoshi/environment_profile.py
@@ -1,13 +1,6 @@
 """
 This is for gathering information about the python environment and
 determining function scope and calls
-
-Todo:
-    * find a way to get the modules imported from the script
-      that imports this one
-    * write function to validate if a function profiled
-      is in scope
-    * if a funciton isn't in scope find a module that has it (?)
 """
 
 import ast
@@ -32,26 +25,12 @@ def get_arg_label(my_arg):
         return my_arg.s
 
 
-def imports():
-    """
-    Get a list of the currently imported modules
-    """
-
-    for _, val in globals().items():
-        if isinstance(val, types.ModuleType):
-            yield val.__name__
-
-
 # pylint: disable=too-few-public-methods
 class ProfiledFunction(object):
     """
     Profiled function class for easily handing information
     and checks to see if it is in scope/what module
     the function could belong to.
-    Todo:
-        - fix argument parsing
-        - Write function for detecting if in scope
-        - Write function to get module infomration
     """
 
     def __init__(self, args, keywords, function_id):
@@ -61,18 +40,6 @@ class ProfiledFunction(object):
         kword_tup = [(x.arg, x.value.s) for x in keywords]
         self.keywords = dict((x, y) for x, y in kword_tup)
         self.function_id = function_id
-
-    def check_import_status(self):
-        """
-        Check whether or not the function is part of an imported
-        module in scope
-        """
-
-        print([x for x in imports()])
-        if builtins.any(hasattr(
-                module_name, self.function_id) for module_name in imports()):
-            return True
-        return False
 
 
 def parse_function(function_string):
